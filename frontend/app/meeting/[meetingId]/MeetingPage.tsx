143fb5b04d08225b20f217468632b285607b345c
Sagnik Das <sagnikdasso4@gmail.com>
2026-07-19 01:21:53 +0530

    Remove dynamic displayName and set to 'Guest'

--- frontend/app/meeting/[meetingId]/MeetingPage.tsx
+++ frontend/app/meeting/[meetingId]/MeetingPage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { use } from "react";
-import { useSearchParams } from "next/navigation";
 import MeetingRoom from "@/components/MeetingRoom";
 
 interface MeetingPageProps {
@@ -10,13 +9,11 @@ interface MeetingPageProps {
 
 export default function MeetingPage({ params }: MeetingPageProps) {
   const { meetingId } = use(params);
-  const searchParams = useSearchParams();
-  const displayName = searchParams.get("name") || "Guest";
 
   return (
     <MeetingRoom
       meetingId={meetingId}
-      displayName={displayName}
+      displayName="Guest"
       isHost={false}
     />
   );
